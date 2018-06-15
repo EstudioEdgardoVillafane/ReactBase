ee35ca4f23b5ebf5249d6721ed29b2cf31581efb
CristianHourcade <hourcadecristian@gmail.com>
2018-06-15 12:54:24 -0300

    V4 - Folder img in public folder

--- src/components/Global/Header/index.js
+++ src/components/Global/Header/index.js
@@ -12,17 +12,25 @@ class Header extends Component {
   render() {
   
     return (
+      <div>
       <div className="Header">
         <div className="container">
-          <div className="row">
-            <div className="col-6">
-            Compania S.R.C
+          <div className="row pt-3">
+            <div className="col-4">
+              <p className="">
+                Compania S.R.C
+              </p>
             </div>
-            <div className="col-6">
-            toto toto tootx
+            <div className="offset-4 col-4 ">
+              <a className="ml-auto">Home</a>
+              <a className="ml-4">About</a>
+              <a className="ml-4">Contact</a>
             </div>
           </div>
         </div>
+        <div className="src-fondo">
+        </div>
+      </div>
       </div>
     );
   }
